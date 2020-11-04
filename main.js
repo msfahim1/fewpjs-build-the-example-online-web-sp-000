2185f0d8f663d2251190eb009890356f78282024
Michael Fahim <michael_fahim@yahoo.com>
2020-11-04 23:01:10 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,6 +3,52 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector("#modal")
+modal.setAttribute("class", "hidden")
+
+
+
+let likes = document.querySelectorAll("span.like-glyph")
+likes.forEach(like => {
+  like.addEventListener("click", function() {
+    if(like.innerHTML === `${FULL_HEART}`) {
+
+      fullHeart(like)
+    } else {
+
+      mimicServerCall()
+      .then(() => {
+       emptyHeart(like)
+      })
+
+      .catch((error) => {
+        modal.removeAttribute("class", "hidden")
+        modal.innerHTML = error
+        setTimeout(function() {
+          modal.setAttribute("class", "hidden")
+        }, 5000)
+      })
+    }
+  })
+})
+
+function emptyHeart(like) {
+  like.innerHTML = `${FULL_HEART}`
+  // like.className = "activated-heart"
+  like.setAttribute("class", "activated-heart")
+}
+
+function fullHeart(like) {
+  like.innerHTML = `${EMPTY_HEART}`
+  like.classList.remove("activated-heart")
+}
+
+
+
+
+
+
+
 
 
 
